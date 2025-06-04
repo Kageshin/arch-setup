1ff250d1b2af105ddc774e0e9288b877461716da
Kageshin <22819283+Kageshin@users.noreply.github.com>
2025-06-04 20:38:10 +0200

    Bootstrap Script zur Automatisierten Installation

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "[1/5] Pacman vorbereiten..."
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed git base-devel ansible stow --noconfirm
+
+echo "[2/5] Projects Ordern erstellen..."
+mkdir -p ~/Projects
+
+echo "[2/5] Repo klonen..."
+#git clone https://github.com/.... ~/Projects/arch-setup
+
+echo "[4/5] Dotfiles vorbereiten..."
+cd ~/Projects/arch-setup/dotfiles
+stow *
+
+echo "[5/5] Ansible ausführen..."
+cd ~/Projects/arch-setup/ansible
+ansible-playbook playbook.yml -K
