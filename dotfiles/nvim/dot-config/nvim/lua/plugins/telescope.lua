d69e82a05a82286f09466b317a267903e04cab30
Kageshin <22819283+Kageshin@users.noreply.github.com>
2025-06-11 22:26:44 +0200

    Dotfile, nvim: lazy.nvim Setup + Telescope Start Pkglist: ripgrep (Suche nach Regex in Datein)

---
+++ dotfiles/nvim/dot-config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+	"nvim-telescope/telescope.nvim", tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = function()
+		local builtin = require("telescope.builtin")
+		return {
+			{ 
+				"<leader>pf", 
+				function() builtin.find_files() end, 
+				mode = "n", 
+				desc = "Telescope find files" 
+			},
+			{ 
+				"<C-p>", 
+				function() builtin.git_files() end,	
+				mode = "n", 
+				desc = "Telescope git files" 
+			},
+			{ 
+				"<leader>ps", 
+				function() builtin.grep_string({ search = vim.fn.input("Grep > ")}) end,	
+				mode = "n", 
+				desc = "Telescope grep string in files" 
+			},
+
+		}
+	end,
+
+	config = function()
+		require("telescope").setup{}
+	end
+}
+
+--local builtin = require('telescope.builtin')
+--vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
