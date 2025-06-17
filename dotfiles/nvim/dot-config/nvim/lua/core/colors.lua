818a627587424b4eacf1edad5128beeefd2be95c
Kageshin <22819283+Kageshin@users.noreply.github.com>
2025-06-17 23:15:42 +0200

    nvim: Fixed Theme when terminal supports truecolor

--- dotfiles/nvim/dot-config/nvim/lua/core/colors.lua
+++ dotfiles/nvim/dot-config/nvim/lua/core/colors.lua
@@ -1,8 +1,8 @@
 -- Fallback colorscheme when truecolor support is missing
 vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		if vim.fn.has("termguicolors") == 1 then
-			vim.cmd("colorscheme vim")
-		end
-	end,
+    callback = function()
+        if not vim.opt.termguicolors then
+            vim.cmd("colorscheme vim")
+        end
+    end,
 })
--- dotfiles/nvim/dot-config/nvim/lua/plugins/rose-pine.lua
+++ dotfiles/nvim/dot-config/nvim/lua/plugins/rose-pine.lua
@@ -4,7 +4,7 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     priority = 1000,
-    enabled = vim.fn.has("termguicolors") == 0,
+    enabled = vim.fn.exists('+termguicolors') == 1,
 
     config = function()
         vim.o.termguicolors = true
